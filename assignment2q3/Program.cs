734cfd44b7f97f83b1902d0b5eaf1ad1e4979f2d
Nehash <nehash.j.m027@gmail.com>
2023-11-08 16:25:47 +0530

    assignment upload

---
+++ assignment2q3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment2q3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.WriteLine("enter a number");
+            a = int.Parse(Console.ReadLine());
+            if(a>0)
+            {
+                Console.WriteLine("a is positive number");
+                if (a % 2 == 0)
+                {
+                    Console.WriteLine("a is even");
+                }
+                else
+                {
+                    Console.WriteLine("a is odd");
+                }
+            }
+            else if (a < 0)
+            {
+                Console.WriteLine("a is negetive number");
+            }
+            else
+            {
+                Console.WriteLine("a is zero");
+            }
+            Console.ReadKey();
+        }
+    }
+}
